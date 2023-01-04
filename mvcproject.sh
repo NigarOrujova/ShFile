13de1097cac48e85471ac250443c85c6ecb569ac
Nigar Orucova <92165280+Norucova@users.noreply.github.com>
2023-01-04 12:57:04 +0400

    Task done

--- mvcproject.sh
+++ mvcproject.sh
@@ -1,5 +1,29 @@
 
 file=${PWD##*/}
 mkdir $file
+cd $file
+dotnet new sln
 dotnet new mvc -lang C# -n $file
+dotnet sln $file.sln add **/*.csproj
+cd "$file"
+dotnet add package Microsoft.EntityFrameworkCore.Tools --version 7.0.1
+dotnet add package Microsoft.EntityFrameworkCore.SqlServer --version 7.0.1
+mkdir Models/Entities
+cd Models/Entities
+echo "namespace $file.Models.Entities
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+    }
+}" > Product.cs
 
+echo "namespace $file.Models.Entities
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+    }
+} " > Category.cs
